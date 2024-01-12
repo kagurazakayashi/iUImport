787aab8a79aec2dfc2fda3f31c1d73e906cd4e26
神楽坂雅詩 <cyscxchope@gmail.com>
2024-01-12 20:33:17 +0800

    创建空白工程

---
+++ iuimport/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  iuimport
+//
+//  Created by 神楽坂雅詩 on 2024/3/2.
+//
+
+// 匯入 SwiftUI 框架
+import SwiftUI
+
+class FileModel: Identifiable {
+    var name: String?
+    var info: String?
+    init(name: String? = nil, info: String? = nil) {
+        self.name = name
+        self.info = info
+    }
+}
+
+// 結構體 ContentView 繼承自 View 協定
+struct ContentView: View {
+    @State var files = [FileModel(name: "行1", info: "行1内容"),
+                         FileModel(name: "行2", info: "行2内容")]
+    var body: some View {
+        NavigationView {
+            List(files) { file in
+                HStack {
+                    Text(file.name ?? "")
+                    Text(file.info ?? "")
+                }
+            }
+            .toolbar {
+                Button(action: {
+                    // TODO: 选择文件
+                }) {
+                    Text("选择文件")
+                }
+            }
+        }
+        .onAppear {
+            // 加载文档
+        }
+    }
+}
+
+// 預覽
+#Preview {
+    // 顯示 ContentView 視圖
+    ContentView()
+}
---
+++ iuimport/iuimportApp.swift
@@ -0,0 +1,17 @@
+//
+//  iuimportApp.swift
+//  iuimport
+//
+//  Created by 神楽坂雅詩 on 2024/3/2.
+//
+
+import SwiftUI
+
+@main
+struct iuimportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
