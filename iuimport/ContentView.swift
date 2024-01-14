9db87dd90035d532bd9ac83dab1c6d9d43551fbd
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2024-01-14 20:49:07 +0800

    获取图片和视频的缩略图

--- iuimport/ContentView.swift
+++ iuimport/ContentView.swift
@@ -8,25 +8,53 @@
 // 匯入 SwiftUI 框架
 import SwiftUI
 
-class FileModel: Identifiable {
-    var name: String?
-    var info: String?
-    init(name: String? = nil, info: String? = nil) {
-        self.name = name
-        self.info = info
-    }
-}
-
 // 結構體 ContentView 繼承自 View 協定
 struct ContentView: View {
     let mgr = DocumentsManager()
-    @State var files = [FileModel]()
+    @State var files:[FileInfoModel] = [FileInfoModel]()
     var body: some View {
         NavigationView {
             List(files) { file in
                 HStack {
-                    Text(file.name ?? "")
-                    Text(file.info ?? "")
+                    let extName:String = mgr.getExtension(fromPath: file.path)
+                    let iconName:String = mgr.getIconName(extName: extName)
+                    if (iconName == "photo") {
+                        if let image = UIImage(named: file.path) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                        } else {
+                            Image(systemName: iconName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                        }
+                    } else if (iconName == "video") {
+                        if let image = mgr.videoThumbnail(from: file.path) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                        } else {
+                            Image(systemName: iconName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                        }
+                    } else {
+                        Image(systemName: iconName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50)
+                    }
+                    VStack(alignment: .leading) {
+                        Text(file.name).font(.title2)
+                        Text(mgr.formatFileSize(size: file.size)+" | "+mgr.dateToString(date: file.modification)).font(.caption)
+                    }
+                    Spacer()
+//                    Image(systemName: "checkmark")
+                    ProgressView()
                 }
             }
             .toolbar {
@@ -39,7 +67,7 @@ struct ContentView: View {
         }
         .onAppear {
             // 加载文档
-            
+            files = mgr.getDocumentsFiles()
         }
     }
 }
--- iuimport/DocumentsManager.swift
+++ iuimport/DocumentsManager.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+import AVFoundation
 
 class FileInfoModel: Identifiable {
     var name: String = ""
@@ -43,4 +45,62 @@ class DocumentsManager {
         print(allFiles)
         return allFiles
     }
+    
+    func formatFileSize(size: Int, base: Double = 1024.0) -> String {
+        let units:[String] = ["", "K", "M", "G", "T", "P", "E"]
+        var fileSize:Double = Double(size)
+        var index:Int = 0
+        while fileSize >= base && index < units.count - 1 {
+            fileSize /= base
+            index += 1
+        }
+        return String(format: "%.2f %@B", fileSize, units[index])
+    }
+    
+    func dateToString(date: Date, style:DateFormatter.Style = DateFormatter.Style.full, locale:Locale = Locale.current) -> String {
+        let dateFormatter:DateFormatter = DateFormatter()
+        dateFormatter.locale = locale
+        dateFormatter.dateStyle = style
+        return dateFormatter.string(from: date)
+    }
+    
+    func getExtension(fromPath path: String) -> String {
+        let fileName = URL(fileURLWithPath: path).lastPathComponent
+        let components = fileName.components(separatedBy: ".")
+        guard components.count > 1 else {
+            return ""
+        }
+        return components.last ?? ""
+    }
+    
+    func getIconName(extName: String) -> String {
+        var iconName = "doc"
+        switch extName {
+        case "heif", "jpg", "jpeg", "raw", "png", "gif", "tiff", "jfif":
+            // https://support.apple.com/en-us/108314
+            iconName = "photo"
+            break;
+        case "hevc", "mp4", "mov":
+            iconName = "video"
+            break;
+        default:
+            break;
+        }
+        return iconName
+    }
+    
+    func videoThumbnail(from path: String) -> UIImage? {
+        let videoURL = URL(fileURLWithPath: path)
+        let asset = AVAsset(url: videoURL)
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        imageGenerator.appliesPreferredTrackTransform = true
+        let time = CMTime(seconds: 0, preferredTimescale: 600)
+        var actualTime: CMTime = CMTime()
+        guard let imageRef = try? imageGenerator.copyCGImage(at: time, actualTime: &actualTime) else {
+            return nil
+        }
+//        let image = UIImage(cgImage: imageRef)
+        return UIImage(cgImage: imageRef)
+    }
+
 }
